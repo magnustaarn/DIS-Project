43d11bad5bd618f10cd2d3ce09c47dd626b8f175
unknown <magnus.taarnberg@gmail.com>
2026-06-06 11:34:04 +0200

    key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-change-in-production")
+    app.secret_key = os.environ.get("123abc")
 
     from game import game_bp
     app.register_blueprint(game_bp)
